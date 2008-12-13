452ebd2b904170958e90f7b1ad8122346bed7c03
petteri.reunamo <petteri.reunamo@a4962020-8624-11dd-b2bb-736fb4d8a922>
2008-12-13 21:20:27 +0000

    INSERT kierto toimii

--- dxf/helpers.py
+++ dxf/helpers.py
@@ -7,11 +7,13 @@ def rotate_points(x, y, angle):
         return x_tmp, y_tmp
 
 def rotate(canvas, tag, angle):
-    pnts = canvas.coords(tag)
-    rotated = [tag]
-    for x,y in zip(pnts[0:len(pnts):2], pnts[1:len(pnts):2]):
-        x,y = rotate_points(x, y, angle)
-        rotated.append(x)
-        rotated.append(y)
-    canvas.coords(*rotated)
+    tags =  canvas.find_withtag(tag)
+    for tag in tags:
+            pnts = canvas.coords(tag)
+            rotated = [tag]
+            for x,y in zip(pnts[0:len(pnts):2], pnts[1:len(pnts):2]):
+                x,y = rotate_points(x, y, angle)
+                rotated.append(x)
+                rotated.append(-y)
+            canvas.coords(*rotated)
     
