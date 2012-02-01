ba2dc0996335e66f048e18879cbbb9870e08023e
petteri.reunamo <petteri.reunamo@gmail.com>
2012-02-01 05:52:47 +0000

    koekoe

---
+++ default_tools/draw.py
@@ -0,0 +1,31 @@
+import current
+import cad_canvas
+
+
+def __get_point(event=None):
+    return  current.canvas.getPoint()
+
+def point(event=None):
+    p = __get_point()
+    cad_canvas.vp.Point(current.canvas, current.db, "P", p)
+
+def points(event=None):    
+    p = __get_point()
+    while p:
+        cad_canvas.vp.Point(current.canvas, current.db, "P", p)
+        p = __get_point()
+
+def lines(event=None):
+    sp = __get_point()
+    ep = __get_point()
+    while ep:
+        l = cad_canvas.vp.Line(current.canvas, current.db, "L", sp+ep)
+        l.repaint()
+        current.canvas.SwapBuffers()
+        sp = ep
+        ep = __get_point()
+    
+        
+            
+
+
