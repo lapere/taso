f0dfa6b4d9b3a445437c480aaedc3cef241d25cb
petteri.reunamo <petteri.reunamo@gmail.com>
2012-02-01 05:52:47 +0000

    koekoe

---
+++ cad_canvas/default_tools/level.py
@@ -0,0 +1,121 @@
+from Tkinter import *
+from item import VisualYItem, VisualXItem, VisualAngleItem     
+from utils import *
+
+
+
+def solve_line(self, other):
+    
+    nim = self.A() * other.B() - other.A() * self.B()
+    
+    if not nim:
+        return 0, 0
+        
+    x_os = -self.C() *  other.B() - -other.C() *  self.B()
+    y_os =  self.A() * -other.C() - other.A() * -self.C()
+
+    x = float(x_os) / nim
+    y = float(y_os) / nim
+
+    return x, y
+    
+
+
+class _X(VisualXItem):
+
+    def __init__(self, canvas, value):
+        VisualXItem.__init__(self, canvas, "X", value)
+        self.line_list_param = [value, 0, value, canvas["height"]]
+
+        self.active_fill = dict(fill="red")
+        self.passive_fill = dict(fill="gray")
+        self.selected_fill = dict(fill="green")            
+
+        self.line_kw_param = dict(width=1, fill="gray", tags=self.tag)
+        self.create_visibles()
+        
+                
+    def create_visibles(self):
+        
+        self.id = self.canvas.create_line(self.line_list_param,
+                                          self.line_kw_param)
+        self.canvas.tag_lower(self.tag)
+        self.bindit()    
+        return self.id
+
+    def repaint(self):
+        self.canvas.coords(self.id, float(self.value), 0, float(self.value), self.canvas["height"])
+            
+        
+
+class _Y(VisualYItem):
+
+    def __init__(self, canvas, value):
+        VisualYItem.__init__(self, canvas, "Y", value)
+        self.line_list_param = [0, value, canvas["width"], value]
+
+        self.active_fill = dict(fill="red")
+        self.passive_fill = dict(fill="gray")
+        self.selected_fill = dict(fill="green")            
+
+        self.line_kw_param = dict(width=1, fill="gray", tags=self.tag)
+        self.create_visibles()
+        
+                
+    def create_visibles(self):
+        
+        self.id = self.canvas.create_line(self.line_list_param,
+                                          self.line_kw_param)
+        self.canvas.tag_lower(self.tag)
+        self.bindit()    
+        return self.id
+
+    def repaint(self):
+        self.canvas.coords(self.id, 0, float(self.value), self.canvas["width"], float(self.value))
+
+
+class _A(VisualAngleItem):
+
+    def __init__(self, canvas, x, y, a):
+        self.line_list_param = [0,100,200,300]
+        VisualAngleItem.__init__(self, canvas, "A", x, y, a)
+    
+    
+        self.active_fill = dict(fill="red")
+        self.passive_fill = dict(fill="gray")
+        self.selected_fill = dict(fill="green")            
+
+        self.line_kw_param = dict(width=1, fill="gray", tags=self.tag)
+        self.create_visibles()
+        
+                
+    def create_visibles(self):
+        
+        self.id = self.canvas.create_line(self.line_list_param,
+                                          self.line_kw_param)
+        self.canvas.tag_lower(self.tag)
+        self.bindit()    
+        return self.id
+    
+    def repaint(self):
+
+        _x = float(self.value.x)
+        _y = float(self.value.y)
+        a = float(self.value)
+
+        print _x,_y,a
+
+        if cos(a) and sin(a):
+            l = [_x - cos(a) * 10,
+                 _y + sin(a) * 10,
+                 _x + cos(a) * 10,
+                 _y - sin(a) * 10]
+        elif not sin(a):
+            l = [_x, _y - 10, _x, _y + 10]
+        else:
+            l = [_x - 10, _y, _x + 10, _y]
+        m = float(max(self.canvas["width"], self.canvas["height"]))
+        self.canvas.coords(self.id, l[0], l[1], l[2], l[3])        
+        self.canvas.scale(self.id, _x, _y, m/10, m/10)
+        
+
